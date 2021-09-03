cdeea6007ba47bcfe96eb8a7438685a4948938b7
tloveday <thomas_loveday848@hotmail.com>
2021-09-03 16:44:31 +0000

    Addition of the Television section

--- app.py
+++ app.py
@@ -25,6 +25,12 @@ def get_movies():
     return render_template("movies.html", movies=movies)
 
 
+@app.route("/get_television")
+def get_television():
+    tvshows = mongo.db.tvshows.find()
+    return render_template("tvshows.html", tvshows=tvshows)
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
