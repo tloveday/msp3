50edc7206425843abfd392edcb25b1da7b40ff01
tloveday <thomas_loveday848@hotmail.com>
2021-09-04 18:36:12 +0000

    Register function added

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import (
     Flask, flash, render_template,
-    redirect, session, url_for)
+    redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -19,23 +19,45 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 
+# Home Page
 @app.route("/")
 @app.route("/landing")
 def landing():
     return render_template("index.html")
 
 
+# Registration Page
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # Check if username already exists in database
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' through cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful - Welcome to Level 7!")
     return render_template("register.html")
 
 
+# Movies Page
 @app.route("/get_movies")
 def get_movies():
     movies = mongo.db.movies.find()
     return render_template("movies.html", movies=movies)
 
 
+# TV Shows Page
 @app.route("/get_television")
 def get_television():
     tvshows = mongo.db.tvshows.find()
