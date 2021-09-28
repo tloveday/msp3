8c10162a50de0c25ba59e0fc8cce58a6cb6ad97b
tloveday <thomas_loveday848@hotmail.com>
2021-09-28 11:50:46 +0000

    Addition of Star Rating and JQuery Click

--- static/js/script.js
+++ static/js/script.js
@@ -7,3 +7,15 @@ $(document).ready(function(){
 
 
 //Guide for making the Star Rating System in JQuery from Happy Coder on Youtube - https://www.youtube.com/watch?v=1xW6DPpwcDM&t=14s
+var a;
+var clicked;
+
+
+$('.rating-star').click(function(){
+    $('.rating-star').removeClass('rating-hover rating-picked');
+    
+    $(this).removeClass('far').addClass('rating-picked fas');
+    $(this).prevAll().addClass('rating-picked fas');
+    $(this).nextAll().addClass('fas')
+
+})
