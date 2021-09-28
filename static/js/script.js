157c646fed93ebb7e6afcc2ba8da6ae08e8a9771
tloveday <thomas_loveday848@hotmail.com>
2021-09-28 12:03:32 +0000

    Addition of Star hover

--- static/js/script.js
+++ static/js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 })
 
 
-//Guide for making the Star Rating System in JQuery from Happy Coder on Youtube - https://www.youtube.com/watch?v=1xW6DPpwcDM&t=14s
+//Guide for making the Star Rating System in JQuery from Happy Coder on Youtube - https://www.youtube.com/watch?v=1xW6DPpwcDM&t=14s edited to meet design
 var a;
 var clicked;
 
@@ -16,6 +16,30 @@ $('.rating-star').click(function(){
     
     $(this).removeClass('far').addClass('rating-picked fas');
     $(this).prevAll().addClass('rating-picked fas');
-    $(this).nextAll().addClass('fas')
+    $(this).nextAll().addClass('far');
 
-})
+    a = $(this).index();
+
+
+    clicked == $($('.rating-star').get(a));
+
+    $(clicked).data('clicked', true);
+
+});
+
+$('.rating-star').hover(function(){
+    $('.rating-star').removeClass('rating-hover rating-picked');
+    
+    $(this).addClass('rating-hover');
+    $(this).prevAll().addClass('rating-hover');
+
+});
+
+$('rating-star').mouseout(function(){
+    $('.rating-star').removeClass('rating-hover');
+
+    if ($(clicked).data('clicked')){
+        $(clicked).addClass('rating-picked');
+        $(clicked).prevAll().addClass('rating.chosen');
+    }
+});
